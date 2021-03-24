56b3a395222ad22575754a92fa3cbe275dd51f07
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-03-24 15:35:51 -0300

    criaçao do arquivo exercicio2.js e exercicio1 finalizado

---
+++ bloco_4/dia_1/exercicio1.js
@@ -0,0 +1,46 @@
+// Exercicio 1 
+// Adição
+
+// let a = 10;
+// let b = 5;
+
+// let resultado = a + b;
+
+// console.log(resultado);
+
+// Subtração
+
+// let a = 10;
+// let b = 5;
+
+// let resultado = a - b;
+
+// console.log(resultado);
+
+// Multiplicação
+
+// let a = 10;
+// let b = 5;
+
+// let resultado = a * b;
+
+// console.log(resultado);
+
+
+// Divisão
+
+// let a = 10;
+// let b = 5;
+
+// let resultado = a / b;
+
+// console.log(resultado);
+
+// Módulo
+
+let a = 10;
+let b = 5;
+
+let resultado = a ** b;
+
+console.log(resultado);
