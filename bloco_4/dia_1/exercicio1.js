35a20b9c1e51ee357c24aba641ebb34aa8f8a769
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-03-24 15:47:08 -0300

    criaçao do exercicio3.js e finalizaçao do mesmo

--- bloco_4/dia_1/exercicio1.js
+++ bloco_4/dia_1/exercicio1.js
@@ -1,4 +1,4 @@
-// Exercicio 1 
+
 // Adição
 
 // let a = 10;
@@ -44,3 +44,5 @@ let b = 5;
 let resultado = a ** b;
 
 console.log(resultado);
+
+
---
+++ bloco_4/dia_1/exercicio3.js
@@ -0,0 +1,16 @@
+let a = 25; 
+let b = 311;
+let c = 420;
+
+if (a > b && a > c)
+{
+  console.log ("O maior valor digitado é" , a);
+}
+else if (b > a && b > c)
+{
+  console.log ("O maior valor digitado é" , b);
+}
+else if (c > a && c > b)
+{
+  console.log ("O maior valor digitado é" , c);
+}
