0cded5f23e1bcae1179b0f58a47f813f5c167e58
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-03-27 13:37:30 -0300

    exercicios 3 e 4 de array e loop finalizados

---
+++ bloco_4/dia_2/array-loop_ex03.js
@@ -0,0 +1,10 @@
+let numbers = [70, 100];
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1){
+  sum += numbers[index] 
+
+}
+let average = sum  / numbers.length;
+console.log(average);
+
+
