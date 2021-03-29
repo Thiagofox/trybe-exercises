a2252bc7785d1a5e7913223dc3e21d2737807274
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-03-29 10:23:18 -0300

    exercicio 1 de LP finalizado

---
+++ bloco_4/dia_3/ex-lp01.js
@@ -0,0 +1,14 @@
+let n = 8;
+let symbol = '*';
+let inputLine = '';
+
+for (index = 0; index < n; index += 1){
+  inputLine = inputLine + symbol;
+}
+  for (let secondIndex = 0; secondIndex < n; secondIndex += 1){
+    console.log(inputLine);
+
+  }
+    
+
+
