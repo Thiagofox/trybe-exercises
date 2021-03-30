f16de6e23ad8355f33423a14fe489fe331a04edb
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-03-30 10:53:49 -0300

    exercicio 5 finalizado de verdade

--- bloco_4/dia_4/exercicios_functions05.js
+++ bloco_4/dia_4/exercicios_functions05.js
@@ -1,23 +1,31 @@
-let array = [2, 3, 2, 5, 8, 2, 3];
+let numbers = [2, 3, 2, 5, 8, 2, 3];
 
-let countRep = 0;
-let countNum = 0;
-let indexNumRep = 0;
+function repeatNumbers (array){
+  
+  let countRep = 0;
+  let countNum = 0;
+  let indexNumRep = 0;
 
-for (let index = 0; index < array.length; index += 1){
-  let numberVer = array[index];
+  for (let index = 0; index < array.length; index += 1){
+    let numberVer = array[index];
 
-  for(let index2 = 0; index2 < array.length; index2 += 1){
-    if (numberVer === array[index2]){
-      countNum += 1;
+    for(let index2 = 0; index2 < array.length; index2 += 1){
+      if (numberVer === array[index2]){
+        
+        countNum += 1;
 
+      }
     }
+    
+    if(countNum > countRep){
+      countRep = countNum;
+      indexNumRep = array[index];
+    }
+    countNum = 0;
   }
-  if(countNum > countRep){
-    countRep = countNum;
-    indexNumRep = array[index];
-  }
-  countNum = 0;
+  return(indexNumRep)
 }
-console.log(indexNumRep)
+
+console.log(repeatNumbers(numbers));
+
 
