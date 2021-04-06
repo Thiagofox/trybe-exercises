2cbb0d17c3263a65f34363a25fd7d747c89efc92
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-05 22:11:10 -0300

    enviando bloco 4.4 final

--- bloco_4/dia_3/ex-lp01.js
+++ bloco_4/dia_3/ex-lp01.js
@@ -5,10 +5,10 @@ let inputLine = '';
 for (index = 0; index < n; index += 1){
   inputLine = inputLine + symbol;
 }
-  for (let secondIndex = 0; secondIndex < n; secondIndex += 1){
-    console.log(inputLine);
+for (let secondIndex = 0; secondIndex < n; secondIndex += 1){
+  console.log(inputLine);
 
-  }
+}
     
 
 
