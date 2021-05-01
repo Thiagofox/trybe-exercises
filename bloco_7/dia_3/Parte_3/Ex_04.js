fb42408c1206bea75ad259ba554ad6b19c357067
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-01 09:30:01 -0300

    exercicio final bloco 7 dia 3

--- bloco_7/dia_3/Parte_3/Ex_04.js
+++ bloco_7/dia_3/Parte_3/Ex_04.js
@@ -2,26 +2,21 @@
 
 // 4 - Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
+const assert = require('assert');
+
 function secondThirdSmallest(array) {
-  let results = []
+  let result = []
   array.sort((function (a, b){return a - b}));
-  array[]
-      
-
-  
+  result.push(array[1]);
+  result.push(array[2]);
+  return result;
 };
-
-
-
 console.log(secondThirdSmallest([4, 10, 32, 9, 21, 90, 5, 11, 8, 6]))
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
-parameter.sort(function (a, b){return a - b});
-console.log(parameter);
-
-
-
 const result = [5, 6];
 
+assert.strictEqual(typeof secondThirdSmallest, 'function');
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
 
 // =======================================================================
