83966c947e3575f0ab359564bd16c02d250a3f83
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-01 14:17:40 -0300

    finalizando parte 1 e bonus

--- bloco_8/dia_1/Exercicio_Parte01.js
+++ bloco_8/dia_1/Exercicio_Parte01.js
@@ -20,13 +20,16 @@ const pessoa = (name) => {
 
 console.log(newEmployees(pessoa));
 
+// ===========================================================================>
+
+
 /*
 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 */
 
 
 const verificaSorte = (numSorteio, action) => {
-  const number = Math.floor(Math.random() * 5);
+  const number = (Math.floor(Math.random() * 5) + 1);
   return action(numSorteio, number);
 }
 
@@ -39,4 +42,18 @@ const checkSorteio = (numSorteio, number) => {
 
 verificaSorte(2, checkSorteio);
 
+// usando if ternario =/
+
+const numberChecker = (myNumber, number) => 
+myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
+
+// ===========================================================================>
 
--- bloco_8/dia_1/HoFs_exemplo.js
+++ bloco_8/dia_1/HoFs_exemplo.js
@@ -1,3 +1,6 @@
+// Exercicio fixação 01 - bloco 8 - Dia 1 -> High Order Functions
+
+
 const repeat = (number, action) => {
   for (let count = 0; count < number; count += 1) {
     action(count);
@@ -5,3 +8,56 @@ const repeat = (number, action) => {
 };
 
 repeat(5, console.log);
+
+// ===========================================================================>
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+repeat(3, (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+});
+
+// ===========================================================================>
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if ((number % 2) > 0) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat(3, isEven); // Testa quais números serão pares;
+repeat(3, isOdd); // Testa quais números serão ímpares;
+
+// ===========================================================================>
+
+const doingThings = (action) => action();
+
+const acordando = () => console.log(`Acordando!`);
+const cafe = () => console.log(`Tomando Café!`);
+const dormir = () => console.log(`Indo dormir!`)
+
+doingThings(acordando);
+doingThings(cafe);
+doingThings(dormir);
+
+// ===========================================================================>
+
+
