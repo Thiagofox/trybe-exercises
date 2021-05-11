10e241a4ea0f2da055745f0730b2db2e03734a11
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-11 12:45:17 -0300

    Exercicios praticos e de fixação do bloco 9 dia 1 finalizados

---
+++ bloco_9/dia_1/Ex_04.js
@@ -0,0 +1,19 @@
+// Exercicio 4 -> Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature que imprime a temperatura em Marte.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => {
+  const temp = getMarsTemperature();
+  setTimeout (() => console.log(`A Temperatura de Marte ${temp}`), //paramento 1
+    messageDelay() // paramentro 2
+  );
+}
+
+
+
+sendMarsTemperature(getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
