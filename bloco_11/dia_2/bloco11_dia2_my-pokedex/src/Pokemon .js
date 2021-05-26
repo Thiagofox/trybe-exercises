35fa555d3cf8b5944f98aaa818ab381eed697185
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-25 21:33:56 -0300

    mais um commit

---
+++ bloco_11/dia_2/bloco11_dia2_my-pokedex/src/Pokemon .js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class pokemon  extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default pokemon
