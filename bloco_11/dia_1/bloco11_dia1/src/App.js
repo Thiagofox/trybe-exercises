2ba9594370c76c8cd6dcf4f19e63c99fcf1ab8db
thiagofox <thiagofox00@gmail.com>
2021-06-03 20:56:47 -0300

    revisao dia bloco 11-01

--- bloco_11/dia_1/bloco11_dia1/src/App.js
+++ bloco_11/dia_1/bloco11_dia1/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import Teste from './Ex_01';
+import Description from './Component'
 
 function App() {
-  return <Teste />
+  return (
+    <div>
+      <Description />
+      <Teste />
+    </div>
+  ) 
 }
 
 export default App;
