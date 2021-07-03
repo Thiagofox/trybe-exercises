f18f1025fb3c661124d2f23f5bd79490708c89ca
thiagofox <thiagofox00@gmail.com>
2021-07-03 15:47:33 -0300

    exercicio 16.2 caminhando

---
+++ bloco_16/dia_2/aula-ao-vivo-frank/src/components/List.js
@@ -0,0 +1,14 @@
+function list(props) {
+  return (
+    <div>
+      <ul>
+        <li>Minha Primeira Tarefa</li>
+        <li>Minha segunda Tarefa</li>
+      </ul>
+    </div>
+
+       
+  )
+}
+
+export default list 
---
+++ bloco_16/dia_2/aula-ao-vivo-frank/src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+
+
+const INITIAL_STATE = {
+  task: [], 
+  counterTask: 0,
+}
+
+function todoReducer (state = INITIAL_STATE, action) {
+  return state
+}
+
+export default todoReducer
