f69c599a4d8c0d69f0e807a61d8d9bd15d79d5dc
thiagofox <thiagofox00@gmail.com>
2021-07-25 11:08:34 -0300

    setup do Context + hooks corse

---
+++ bloco_18/dia_2/setup-context/src/App.js
@@ -0,0 +1,15 @@
+// Com o Context e o Provider criados, precisamos adicionar o Provider à aplicação:
+
+import React from 'react';
+import ComponenteX from './components/ComponenteX';
+
+
+function App() {
+    return (
+    <main>
+      <ComponenteX />
+    </main>
+  );
+}
+
+export default App;
