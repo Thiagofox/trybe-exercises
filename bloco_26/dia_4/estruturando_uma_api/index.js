8f0fdf77ab50d501db9f5f3deb92dc81e3d12c6b
Thiagofox <thiagofox00@gmail.com>
2021-10-03 15:27:02 -0300

    bloco 26 dia 4 finalizado sem exercicios

--- bloco_26/dia_4/estruturando_uma_api/index.js
+++ bloco_26/dia_4/estruturando_uma_api/index.js
@@ -1,67 +1,66 @@
-const express =  require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+  { id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25 },
+  { id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25 },
 ];
 
-app.get('/recipes', function(_req, res) {
-  res.json(recipes);
+app.get('/recipes', function (req, res) {
+  res.status(200).json(recipes);
+});
+
+app.get('/recipes/pesquisar', function (req, res) {
+  const { name, maxPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.preco < parseInt(maxPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+app.post('/recipes', function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', function (req, res) {
+    return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
 });
 
 app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porte 3001');
+  console.log('Aplicação ouvindo na porta 3001');
 });
-
-// A estrutura básica de uma requisição utilizando o fetch pode ser escrita da seguinte forma:
-
-// fetch('http://localhost:3001/recipes')
-//     .then(resp => resp.json())
-
-// Para dar mais visibilidade, imagine um componente React que precisa exibir essa lista. Ele ficaria mais ou menos assim:
-
-/* 
-class receitasList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [],
-      isLoading: true,
-    };
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/recipes')
-      .then(response => response.json())
-      .then((recipes) => this.setState(
-        { 
-          recipes,
-          isLoading: false,
-        },
-      ));
-  }
-
-  render() {
-    const { recipes, isLoading } = this.state;
-    
-    return (
-      <div>
-        <div>
-          {isLoading && <Loading />}
-          <div className='card-group'>
-            {recipes.map((recipe) => (
-              <div key={recipe.id}>
-                <h1>{recipe.name}</h1>
-                <span>Preço: {recipe.price}</span>
-                <span>Tempo de preparo: {recipe.waitTime}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
-} 
-*/
