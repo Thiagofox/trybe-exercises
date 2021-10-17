d896ec6df00285123f3d40a093754b5be6939966
Thiagofox <thiagofox00@gmail.com>
2021-10-17 09:58:53 -0300

    conteudo corse bloco 27 dia 4

--- bloco_27/dia_4/exercicios_corse/tests/models/movieModelCreate.test.js
+++ bloco_27/dia_4/exercicios_corse/tests/models/movieModelCreate.test.js
@@ -1,32 +1,65 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-/*
-  Como ainda não temos a implementação, vamos fixar
-  um objeto simulando os métodos que iremos desenvolver,
-  porém, eles não terão nenhum comportamento
-*/
-const MoviesModel = {
-  create: () => {}
-};
-
+/* Vamos importar o módulo responsável para abrir a conexão nos nossos models para poder fazer o seu `double`.*/
+const mongoConnection = require('../../models/connection');
+const MoviesModel = require('../../models/movieModel');
+// 
 describe('Insere um novo filme no BD', () => {
+     /* Vamos deixar o objeto com o mock da conexão como uma variável global dentro desse describe. */
+     let connectionMock; 
+
   const payloadMovie = {
-    title: 'Example Movie',
-    directedBy: 'Jane Dow',
-    releaseYear: 1999,
-  }
+     title: 'Example Movie',
+     directedBy: 'Jane Dow',
+     releaseYear: 1999,
+  };
+
+      /* Esta é uma forma provisória para mockar a função insertOne
+      Desta forma ela não vai chamar o banco de verdade para fazer esse teste */
+      before(async () => {
+        const DBServer = new MongoMemoryServer();
+        const URLMock = await DBServer.getUri();
+  
+        connectionMock = await MongoClient
+          .connect(URLMock, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          })
+          .then((conn) => conn.db('model_example'));
+  
+        
+        sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+      });
+  
+
+      /* Restauraremos a função `getConnection` original após os testes. */
+      after(() => {
+        mongoConnection.getConnection.restore();
+      });
 
   describe('quando é inserido com sucesso', () => {
+     
     it('retorna um objeto', async () => {
       const response = await MoviesModel.create(payloadMovie);
 
-      expect(response).to.be.a('object')
-    });
+      expect(response).to.be.a('object');
+     });
 
     it('tal objeto possui o "id" do novo filme inserido', async () => {
       const response = await MoviesModel.create(payloadMovie);
 
-      expect(response).to.have.a.property('id')
+      expect(response).to.have.a.property('id');
     });
-  });
+
+       /* Aqui de fato estamos testando se o filme foi cadastrado após chamar a função `create`.
+    Para isso fizemos uma consulta para o banco para checar se existe um filme com o título cadastrado */
+    it('deve existir um filme com o título cadastrado!', async () => {
+        await MoviesModel.create(payloadMovie);
+        const movieCreated = await connectionMock.collection('movies').findOne({ title: payloadMovie.title });
+        expect(movieCreated).to.be.not.null;
+      });
+   });
 });
