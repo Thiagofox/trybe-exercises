7e1972d596432783c5a2e218299226cb59b8d055
Thiagofox <thiagofox00@gmail.com>
2021-10-25 16:18:27 -0300

    bloco 28 dia 2 finalizado

---
+++ bloco_28/dia_2/ping/exemplosAxios.js
@@ -0,0 +1,38 @@
+// GET
+
+axios.get('/user', {
+  params: {
+    ID: 12345
+  }
+})
+  .then((response) => {
+    console.log(response);
+})
+  .catch((error) => {
+    console.log(error);
+})
+
+// Você pode usar métodos async também
+const getUser = async () => {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// POST
+
+const body = {
+  firstName: 'Fred',
+  lastName: 'Flintstone'
+};
+
+axios.post('/user', body)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
